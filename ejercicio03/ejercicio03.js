b0239c4b7547758ed76b905c169fe4bb9574fba9
stylopm <apmurillo99@hotmail.com>
2022-05-02 20:08:55 -0500

    agrego un if

--- ejercicio03/ejercicio03.js
+++ ejercicio03/ejercicio03.js
@@ -1,22 +1,39 @@
 console.log('Hola desde el ejemplo 03');
 // Ejemplo de switch
-let diaSemana = 4;
+let diaSemana = 6;
 
-switch(diaSemana){
+switch (diaSemana) {
     //( diaSemana===1 )
     case 1:
-    console.log('-----');
-    console.log('Lunes');
-    break;
+        console.log('-----');
+        console.log('Lunes');
+        break;
     case 2:
-    console.log('*******');
-    console.log('Martes');
-    break;
+        console.log('*******');
+        console.log('Martes');
+        break;
     case 3:
-    console.log('///////');
-    console.log('Miercoles');
-    break;
+        console.log('///////');
+        console.log('Miercoles');
+        break;
+    case 4:
+        console.log('|||||||');
+        console.log('Juevs');
+        break;
+    case 5:
+        console.log('^^^^^^^');
+        console.log('Viernes');
+        break;
+    case 6://Sabado
+    case 7://Domingo
+        if(diaSemana === 6){
+            console.log('Es Sabado');
+        } else {
+            console.log('Es domingo');
+        }
+        console.log('día de descanso');
+        break;
     default:
-    console.log('Dia de la semana invalido');
-    break;
+        console.log('Dia de la semana invalido');
+        break;
 }
