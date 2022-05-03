dbc3162f4670366d861fdf2a535b1b60885b9881
stylopm <apmurillo99@hotmail.com>
2022-05-02 19:53:59 -0500

    ejemplo de switch

---
+++ ejercicio03/ejercicio03.js
@@ -0,0 +1,22 @@
+console.log('Hola desde el ejemplo 03');
+// Ejemplo de switch
+let diaSemana = 4;
+
+switch(diaSemana){
+    //( diaSemana===1 )
+    case 1:
+    console.log('-----');
+    console.log('Lunes');
+    break;
+    case 2:
+    console.log('*******');
+    console.log('Martes');
+    break;
+    case 3:
+    console.log('///////');
+    console.log('Miercoles');
+    break;
+    default:
+    console.log('Dia de la semana invalido');
+    break;
+}
