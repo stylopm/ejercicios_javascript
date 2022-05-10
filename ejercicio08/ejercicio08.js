b504033838a0d607ceeea54e17bd43ab60c7582a
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2022-05-09 20:02:18 -0500

    Principios de ECMA 6

---
+++ ejercicio08/ejercicio08.js
@@ -0,0 +1,6 @@
+console.log('Templates');
+// ECMA6 LET CONST
+let fisrtName = 'Luis Antonio'; 
+let age = 37;
+// Plantillas interpolacion ${variable}
+console.log(`Mi nombre es ${fisrtName} y mi edad es ${age}`);
